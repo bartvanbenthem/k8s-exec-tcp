c3226eecf640aa420a177fbb927c5914b1c5aa54
bartvanbenthem <bartvanbenthem@hotmail.com>
2023-10-24 16:31:40 +0200

    update max conn

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ cargo build
 ./target/debug/k8s-pod-exec-tcp-check --ports 22 443 8080 80 \
     --image alpine \
     --hosts 172.22.128.32 172.22.128.33 172.22.128.34 172.22.128.32 172.22.128.99 \
-    --max-connections 15 \
+    --max-connections 10 \
     --namespace test

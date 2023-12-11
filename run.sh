d8436000b525e71ec5bfe88905f87c7934e33820
bartvanbenthem <bartvanbenthem@hotmail.com>
2023-12-11 14:12:34 +0100

    upd run script

--- run.sh
+++ run.sh
@@ -12,9 +12,9 @@ cargo test
 export CC=musl-gcc
 cargo build --target x86_64-unknown-linux-musl --release
 
-./target/x86_64-unknown-linux-musl/release/k8stcp --ports 22 443 53 80 \
+./target/x86_64-unknown-linux-musl/release/k8stcp --ports 22 443 53 37 \
     --image alpine \
-    --hosts 192.168.63.64 172.28.131.13 172.28.131.144 172.28.131.13 8.8.8.8 \
+    --hosts 216.146.35.35 132.163.97.6 129.6.15.28 dns.google \
     --max-connections 20 \
     --service-account default \
     --namespace test
